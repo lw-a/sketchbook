c3106ea3f445f8f2170527e4ea07ec2a73a1a24d
Adam Hemming <hemming.dev@gmail.com>
2022-06-09 10:13:11 +0900

    Generate grid with js

---
+++ script.js
@@ -0,0 +1,20 @@
+let padSize = 500;
+
+function makeGrid(size) {
+    
+    let cellSize = padSize / size;
+
+    for (var i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        for (var j = 0; j < size; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.style.cssText = `height: ${cellSize}px; width: ${cellSize}px; background: red;`;
+            row.appendChild(cell);
+        }
+        document.getElementById("pad").appendChild(row);
+
+    }
+}
+
+makeGrid(16);
