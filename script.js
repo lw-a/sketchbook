c893eb0168af3603ebfb1d60455e5b860a2c0bbb
Adam Hemming <hemming.dev@gmail.com>
2022-06-12 11:26:54 +0900

    Fix grid lines button, add clear all button

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 let padSize = 500;
-let cellColour = "black";
+
+let colourPicker = document.getElementById("colourPicker");
+let cellColour = colourPicker.value;
+drawing(cellColour);
+
 makeGrid(32);
 
+colourPicker.oninput = function () {
+  cellColour = colourPicker.value;
+  drawing(cellColour);
+};
 
+// In case the user is satisfied with the previous they had chosen after using rainbow or eraser
+colourPicker.onclick = function () {
+  cellColour = colourPicker.value;
+  drawing(cellColour);
+};
 
 let slider = document.getElementById("slider");
 let output = document.getElementById("sliderValue");
-slider.oninput = function() {
-    output.innerText = `${this.value}x${this.value}`;
-}
-slider.onchange = function() {
+slider.oninput = function () {
+  output.innerText = `${this.value} x ${this.value}`;
+};
+slider.onchange = function () {
   makeGrid(parseInt(output.textContent));
-}
+};
 
 function deleteGrid() {
   let pad = document.getElementById("pad");
@@ -23,62 +36,56 @@ function deleteGrid() {
 }
 
 function makeGrid(size) {
-  
-    deleteGrid()
-    
-    let cellSize = (padSize / size);
+  deleteGrid();
 
-    for (var i = 0; i < size; i++) {
-        const row = document.createElement("div");
-        for (var j = 0; j < size; j++) {
-            const cell = document.createElement("div");
-            cell.classList.add("cell");
-            cell.style.cssText = `height: ${cellSize}px; width: ${cellSize}px;`;
-            row.appendChild(cell);
-        }
-        document.getElementById("pad").appendChild(row);
+  let cellSize = padSize / size;
 
+  for (var i = 0; i < size; i++) {
+    const row = document.createElement("div");
+    for (var j = 0; j < size; j++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.style.cssText = `height: ${cellSize}px; width: ${cellSize}px;`;
+      row.appendChild(cell);
     }
-  drawing(cellColour);
-} 
+    document.getElementById("pad").appendChild(row);
+  }
 
+  // Sets pen to previous colour when making a new grid if the user was previously using the eraser/white
+  if (cellColour === "white") {
+    cellColour = colourPicker.value;
+  }
+  drawing(cellColour);
+}
 const rainbowButton = document.getElementById("rainbowButton");
-rainbowButton.addEventListener('click', () => {
+rainbowButton.addEventListener("click", () => {
   cellColour = "rainbow";
   drawing(cellColour);
 });
 
-function randomColour () {
-  let maxHex = 0xFFFFFF;
+const clearButton = document.getElementById("clearButton");
+clearButton.addEventListener("click", () => {
+  makeGrid(parseInt(output.textContent));
+});
+
+function randomColour() {
+  let maxHex = 0xffffff;
   let randomNumber = Math.floor(Math.random() * maxHex);
   randomNumber = randomNumber.toString(16);
   return "#" + randomNumber.padStart(6, 0);
 }
 
-let colourPicker = document.getElementById("colourPicker");
-
-// in case user is satisfied with default colour picker value
-colourPicker.onclick = function() { 
-  cellColour = colourPicker.value;
-  drawing(cellColour);
-}
-colourPicker.oninput = function() {
-  cellColour = colourPicker.value;
-  drawing(cellColour);
-}
-
 let eraser = document.getElementById("eraserButton");
 
-eraser.onclick = function() {
+eraser.onclick = function () {
   cellColour = "white";
   drawing(cellColour);
-}
+};
 
 let gridButton = document.getElementById("gridButton");
 gridButton.addEventListener("click", () => {
-  
   toggleGrid();
-  
+
   if (gridButton.innerText === "Grid Off") {
     gridButton.innerText === "Grid On";
   } else {
@@ -86,36 +93,32 @@ gridButton.addEventListener("click", () => {
   }
 });
 
-function toggleGrid () {
-  alert(gridButton.innerText);
+function toggleGrid() {
   let cells = document.querySelectorAll(".cell");
-  
-  if (gridButton.innerText === "Grid On") {
-   cells.forEach((cell) => {
-     cell.style.border = "none";
-   });
-    gridButton.innerText = "Grid Off";
-      } else {
-     cells.forEach((cell) => {
+
+  if (gridButton.innerText === "Grid Off") {
+    cells.forEach((cell) => {
+      cell.style.border = "none";
+    });
+    gridButton.innerText = "Grid On";
+  } else {
+    cells.forEach((cell) => {
       cell.style.border = "solid";
-       cell.style.borderWidth = "0.1px";
-   });
+      cell.style.borderWidth = "0.1px";
+    });
     gridButton.innerText = "Grid Off";
-      }
+  }
 }
 
-function drawing (cellColour) {
- const cells = document.querySelectorAll(".cell");
-cells.forEach((cell) => {
+function drawing(cellColour) {
+  const cells = document.querySelectorAll(".cell");
+  cells.forEach((cell) => {
     cell.addEventListener("mouseover", () => {
-       if (cellColour === "rainbow"){
+      if (cellColour === "rainbow") {
         cell.style.background = randomColour();
-      } 
-      else {
+      } else {
         cell.style.background = cellColour;
       }
-    })
-}) 
+    });
+  });
 }
-
-
